98b5ed41da14e48a7e194ef096cc813881569f32
Séraphin Vandegar <sva@opinum.com>
2018-12-28 11:34:27 -0800

    v0.3.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Package to interact with the Opisense API",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    download_url="https://github.com/opinum/opisense_client/archive/0.3.3.tar.gz",
+    download_url="https://github.com/opinum/opisense_client/archive/0.3.0.tar.gz",
     url="https://github.com/opinum/opisense_client",
     packages=setuptools.find_packages(),
     install_reauires=[
