7d75e463efdea105cba2d9768bafa1a663b89955
Seraphin <38084243+svandegar@users.noreply.github.com>
2019-01-23 15:33:18 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="opisense_client",
-    version="1.0.0",
+    version="1.0.1",
     author="Seraphin Vandegar",
     author_email="svandegar@hotmail.com",
     description="Package to interact with the Opisense API",
